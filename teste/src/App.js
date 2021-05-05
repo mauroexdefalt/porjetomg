2ae7a52fa3df83491d11276ec3d1cdba7a73b315
Mauro <exdefalt951@gmail.com>
2021-05-04 22:55:46 -0300

    inputs

--- teste/src/App.js
+++ teste/src/App.js
@@ -1,12 +1,35 @@
+import { TextField, Button } from '@material-ui/core';
+
 
 function Teste() {
   return (
-    <div>
-      
-      eae gabriel so de boa ?
+    <div >
+      <form style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', marginTop: '5%' }}>
+        
+          <TextField
+            required
+            label="Email"
+            variant="outlined"
+            type='text'
+            placeholder='digite aqui'
+            style={{ margin: '20px' }} />
 
-      eae mauro suave ?
-          </div>
+
+          <TextField
+            required
+            label="Senha"
+            variant="outlined"
+            type='password'
+            placeholder='digite aqui'
+            style={{ margin: '20px' }} />
+
+
+          <Button type='submit' color='primary'>Enviar</Button>
+    
+      </form>
+
+
+    </div>
 
   )
 }
